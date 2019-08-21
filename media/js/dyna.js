80073dd1f9a0f91bf24e7a7068212b64e7e7d176
reganto <rreganto@gmail.com>
2019-08-21 11:22:04 +0430

    add xmlhttprequest

--- media/js/dyna.js
+++ media/js/dyna.js
@@ -5,10 +5,40 @@ function passwordMatch()
 
     if(password != confirm) {
         document.getElementById('msg').innerHTML = 'not matching';
-        document.getElementById('msg').style.color = 'red';
+        document.getElementById('msg').style.color = 'red'; 
 
     } else {
         document.getElementById('msg').innerHTML = 'matching';
         document.getElementById('msg').style.color = 'green';
     }
+}
+
+function xmlhttpPost(strURL) 
+{
+    var xmlHttpReq = false;
+    var self = this;
+
+    self.xmlHttpReq = new XMLHttpRequest();
+    self.xmlHttpReq.open('POST', strURL, true);
+    self.xmlHttpReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    self.xmlHttpReq.onreadystatechange = function() {
+        if(self.xmlHttpReq.readyState == 4) {
+            updatepage(self.xmlHttpReq.responseText);
+        }
+    }
+    self.xmlHttpReq.send(getquesrystring());
+}
+
+function getquesrystring()
+{
+    var form = document.forms['reg'];
+    var username = form.username.value;
+    qstr = 'username=' + escape(username);
+    return qstr;
+}
+
+function updatepage(str)
+{
+    document.getElementById('result').innerHTML = str;
+    document.getElementById('result').style.color = 'red';
 }
