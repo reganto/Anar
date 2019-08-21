861d41b423120cd46281da881011a314e49de12d
reganto <rreganto@gmail.com>
2019-08-21 11:22:29 +0430

    add username ajax urlconf

--- urls.py
+++ urls.py
@@ -1,10 +1,11 @@
 from tornado.web import url
 from handlers.home import HomeHandler
-from handlers.auth import RegisterHandler, LoginHandler, VerifyTokenHandler
+from handlers.auth import RegisterHandler, LoginHandler, VerifyTokenHandler, AjaxHandler
 
 url_patterns = [
     url(r"/", HomeHandler, name='home'),
     url(r"/auth/register", RegisterHandler, name='register'),
     url(r"/v/(\w+)", VerifyTokenHandler, name='vtoken'),
     url(r"/auth/login", LoginHandler, name='login'),
+    url(r"/ajax", AjaxHandler, name='ajax'),
 ]
